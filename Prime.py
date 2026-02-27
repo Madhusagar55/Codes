02d151702db7ee69a061b7dc15ef428d108bdf93
Madhusagar55 <Vtu22938@veltech.edu.in>
2026-02-27 15:16:24 +0530

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -16,9 +16,3 @@ if is_prime(n):
     print("Prime")
 else:
     print("Not Prime")
-
-<<<<<<< HEAD
-=======
-Time complexity: O(√n)
-Space complexity: O(1)
->>>>>>> ccde43cd0da7bf567f9cc533086255e835ae2adb
