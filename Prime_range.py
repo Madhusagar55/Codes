d6693db5e3e52efae28788781f6b49dbb04dbc87
ENUKUSHI MADHUSAGAR <Vtu22938@veltech.edu.in>
2026-03-03 16:07:06 +0530

    Update Prime_range.py

--- Prime_range.py
+++ Prime_range.py
@@ -1,13 +1,20 @@
-def print_primes(start, end):
-    for num in range(start, end + 1):
-        if num > 1:
-            for i in range(2, int(num ** 0.5) + 1):
-                if num % i == 0:
-                    break
-            else:
-                print(num)
+def get_primes(start, end):
+    if start > end:
+        return []
+
+    primes = []
+
+    for num in range(max(2, start), end + 1):
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                break
+        else:
+            primes.append(num)
+
+    return primes
 
 
-start = int(input("Enter a start number:"))
-end = int(input("Enter a end number:"))
-print_primes(start, end)
+start = int(input("Enter start: "))
+end = int(input("Enter end: "))
+
+print(get_primes(start, end))
